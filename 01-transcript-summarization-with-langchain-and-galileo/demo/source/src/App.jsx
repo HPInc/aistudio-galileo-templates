10bded033561a1df5230250aeb2e8bfcacf7753b
Nicky Souza <nickyjhames@hp.com>
2025-04-16 09:57:35 -0300

    Refactoring app to handle api output when there is error

--- 01-transcript-summarization-with-langchain-and-galileo/demo/source/src/App.jsx
+++ 01-transcript-summarization-with-langchain-and-galileo/demo/source/src/App.jsx
@@ -151,9 +151,6 @@ function App() {
 			}
 
 			const jsonResponse = await response.json();
-			console.log("JSON Response:", jsonResponse);
-			console.log("Type of predictions:", typeof jsonResponse.predictions);
-			console.log("Is Array:", Array.isArray(jsonResponse.predictions));
 
 			if (jsonResponse.predictions) {
 				if (Array.isArray(jsonResponse.predictions) && jsonResponse.predictions.length > 0) {
